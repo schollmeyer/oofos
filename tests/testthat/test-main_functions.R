827aeeaf9799917f24c5e13e80e542b64992e55c
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2022-12-11 16:31:42 +0100

    GS:

--- tests/testthat/test-main_functions.R
+++ tests/testthat/test-main_functions.R
@@ -1,11 +1,11 @@
-test_that("multiplication works", {
+test_that("optimize_on_context works", {
 
   context <- compute_random_context(40,6)
   objective <- runif(40)-0.5
   lattice<-compute_concept_lattice(context)
   result_1 <- max(lattice$extents%*%objective)
   model <- optimize_on_context(context,(1:40),objective)
-  result_2 <- gurobi(model,list(outputflag=0))$objval
+  result_2 <- gurobi::gurobi(model,list(outputflag=0))$objval
   result_1==result_2
   expect_equal(result_1, result_2)
 })
